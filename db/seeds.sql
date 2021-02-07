9f6ef6ce12b4f293a9a56d7558f330e72de1ebb9
Snax <shanni.c.suissa@gmail.com>
2021-02-06 23:04:19 -0800

    changed everything from cats to burgers

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, smashed) VALUES ('Mushroom Burger', true);
+INSERT INTO burgers (name, smashed ) VALUES ('Beet Burger', true);
+INSERT INTO burgers (name, smashed) VALUES ('Brocolli Burger', false);
+INSERT INTO burgers (name, smashed) VALUES ('Quinoa Burger', false);
+INSERT INTO burgers (name, smashed) VALUES ('Wow Burger', true);
